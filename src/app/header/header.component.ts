0baf39360b2a997c6c61a29e01f2c876b8a58c87
eobcre <eobcre@gmail.com>
2023-06-30 09:52:56 -0400

    theme switch logic

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  public isLightTheme: boolean = true;
 
+  onThemeSwitchChange(): void {
+    this.isLightTheme = !this.isLightTheme;
+
+    document.body.setAttribute(
+      'data-theme',
+      this.isLightTheme ? 'light' : 'dark'
+    );
+  }
 }
