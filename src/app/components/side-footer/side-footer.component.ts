1993a48a14b8c46b4c174c78dcaa83569cb2c2c9
eobcre <eobcre@gmail.com>
2023-07-01 14:36:41 -0400

    created new component

---
+++ src/app/components/side-footer/side-footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-footer',
+  templateUrl: './side-footer.component.html',
+  styleUrls: ['./side-footer.component.scss']
+})
+export class SideFooterComponent {
+
+}
