518e1f6c615e73a1de7eb3508df0ecd4bc1b1438
eobcre <eobcre@gmail.com>
2023-07-01 14:33:48 -0400

    new components settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { AboutComponent } from './about/about.component';
-import { WorkComponent } from './work/work.component';
-import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './section/header/header.component';
+import { AboutComponent } from './section/about/about.component';
+import { WorkComponent } from './section/work/work.component';
+import { ContactComponent } from './section/contact/contact.component';
+import { ButtonComponent } from './components/button/button.component';
+import { ThemeComponent } from './components/theme/theme.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, AboutComponent, WorkComponent, ContactComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    WorkComponent,
+    ContactComponent,
+    ButtonComponent,
+    ThemeComponent,
+  ],
   imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
