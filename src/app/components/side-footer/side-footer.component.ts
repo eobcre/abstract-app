d0626ee32a7b3c5be6cdecf67ded206c836c245b
eobcre <eobcre@gmail.com>
2023-07-01 14:54:46 -0400

    fontawesome settings

--- src/app/components/side-footer/side-footer.component.ts
+++ src/app/components/side-footer/side-footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-side-footer',
   templateUrl: './side-footer.component.html',
-  styleUrls: ['./side-footer.component.scss']
+  styleUrls: ['./side-footer.component.scss'],
 })
 export class SideFooterComponent {
-
+  faLinkedin = faLinkedin;
 }
