257d9a6f1dfc13163df4318f17f27608df5f2fde
eobcre <eobcre@gmail.com>
2023-07-02 22:10:32 -0400

    component settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './section/header/header.component';
+import { HomeComponent } from './section/home/home.component';
 import { AboutComponent } from './section/about/about.component';
 import { WorkComponent } from './section/work/work.component';
 import { ContactComponent } from './section/contact/contact.component';
@@ -15,6 +16,7 @@ import { SideFooterComponent } from './components/side-footer/side-footer.compon
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
     AboutComponent,
     WorkComponent,
     ContactComponent,
