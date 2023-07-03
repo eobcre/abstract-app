9c4f7d697df62313ca631ed126fc53aacbc63c69
eobcre <eobcre@gmail.com>
2023-07-02 23:25:54 -0400

    data settings

--- src/app/section/work/work.component.ts
+++ src/app/section/work/work.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { workData } from 'src/data/workData';
 
 @Component({
   selector: 'app-work',
   templateUrl: './work.component.html',
-  styleUrls: ['./work.component.scss']
+  styleUrls: ['./work.component.scss'],
 })
 export class WorkComponent {
-
+  workData = workData;
 }
