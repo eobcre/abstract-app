678830f42e7f8012600aeb48908043adef3252c3
eobcre <eobcre@gmail.com>
2023-07-03 23:41:39 -0400

    work data

---
+++ src/data/workData.ts
@@ -0,0 +1,17 @@
+export const workData = [
+  {
+    id: 1,
+    name: 'Cait Salon',
+    src: '../../../assets/work_image/desktop/salon.png',
+  },
+  {
+    id: 1,
+    name: 'Sticky Note',
+    src: '../../../assets/work_image/desktop/note.png',
+  },
+  {
+    id: 1,
+    name: 'Omikuji',
+    src: '../../../assets/work_image/desktop/omikuji.png',
+  },
+];
