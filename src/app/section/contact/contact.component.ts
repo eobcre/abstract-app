ce75b6bbef98902db7233ca2d98127d104b5bfe5
eobcre <eobcre@gmail.com>
2023-07-05 16:41:58 -0400

    props passing logic, emailjs logic

--- src/app/section/contact/contact.component.ts
+++ src/app/section/contact/contact.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  public buttonSend: String = '';
 
+  ngOnInit() {
+    this.buttonSend = 'Send';
+  }
+
+  public sendEmail(e: Event) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_31w637e', 'template_wt0avmk', 'q6DwqqAWkgXu5y16O')
+      .then(
+        (result: EmailJSResponseStatus) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }
 }
