f8893d124477d2c08bd61957906925309da91fd7
eobcre <eobcre@gmail.com>
2023-07-05 16:22:22 -0400

    new component settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './section/header/header.component';
@@ -10,6 +11,7 @@ import { WorkComponent } from './section/work/work.component';
 import { ContactComponent } from './section/contact/contact.component';
 import { FooterComponent } from './section/footer/footer.component';
 import { ButtonComponent } from './components/button/button.component';
+import { ButtonSendComponent } from './components/button-send/button-send.component';
 import { ThemeComponent } from './components/theme/theme.component';
 import { SideFooterComponent } from './components/side-footer/side-footer.component';
 
@@ -23,10 +25,11 @@ import { SideFooterComponent } from './components/side-footer/side-footer.compon
     ContactComponent,
     FooterComponent,
     ButtonComponent,
+    ButtonSendComponent,
     ThemeComponent,
     SideFooterComponent,
   ],
-  imports: [BrowserModule, FontAwesomeModule],
+  imports: [BrowserModule, FontAwesomeModule, SweetAlert2Module.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
